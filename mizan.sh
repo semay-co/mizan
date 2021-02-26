23bc79f8127d793f0e31c0e1b8ab08163bae0e55
Ameer Nuri <penser.co@gmail.com>
2021-02-26 18:39:15 +0300

    fix mizan.sh

--- mizan.sh
+++ mizan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-npm run server --prefix ~/projects/mizan && npm run client --prefix ~/projects/mizan
+npm run server --prefix ~/projects/mizan & npm run client --prefix ~/projects/mizan
 
 
