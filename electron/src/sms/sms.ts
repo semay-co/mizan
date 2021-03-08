71b9fa35fa20f6db1dd2e0badc203ee337281fc2
Ameer Nuri <penser.co@gmail.com>
2021-03-08 07:14:49 +0300

    Merge with origin

--- electron/src/sms/sms.ts
+++ electron/src/sms/sms.ts
@@ -1,9 +1,10 @@
-import puppeteer from "puppeteer"
+import puppeteer from 'puppeteer'
 
 const startBrowser = async () => {
   const browser = await puppeteer.launch({
     headless: true,
   })
+
   const page = await browser.newPage()
 
   return { browser, page }
@@ -15,49 +16,49 @@ const closeBrowser = async (browser: any) => {
 
 const sendSms = async () => {
   const { browser, page } = await startBrowser()
-  const baseUrl = "http://192.168.0.1"
+  const baseUrl = 'http://192.168.0.1'
 
   await page.setViewport({ width: 1265, height: 613 })
 
   const goToLoginPage = async () =>
     Promise.all([
       await page.goto(`${baseUrl}/index.html#login`),
-      await page.waitForNavigation({ waitUntil: "load" }),
+      await page.waitForNavigation({ waitUntil: 'load' }),
     ])
 
   const login = async () => {
     Promise.all([
       await goToLoginPage(),
       await page
-        .type("#txtPwd", "admin")
-        .catch((err) => console.log("writing password failed")),
+        .type('#txtPwd', 'admin')
+        .catch((err) => console.log('writing password failed')),
       await page
-        .click("#btnLogin")
-        .catch((err) => console.log("clicking failed")),
-      await page.waitForNavigation({ waitUntil: "load" }),
+        .click('#btnLogin')
+        .catch((err) => console.log('clicking failed')),
+      await page.waitForNavigation({ waitUntil: 'load' }),
     ])
   }
 
   const goToSmsPage = async () =>
     Promise.all([
       await page.goto(`${baseUrl}/index.html#sms`),
-      await page.click("#smslist-new-sms"),
+      await page.click('#smslist-new-sms'),
     ])
 
   const sendSms = async () =>
     Promise.all([
       await goToSmsPage(),
-      await page.click("#chosen-search-field-input"),
-      await page.type("#chosen-search-field-input", "+251955366856;"),
-      await page.type("#chat-input", "hello from the other side!"),
-      // await page.click('#btn-send'),
+      await page.click('#chosen-search-field-input'),
+      await page.type('#chosen-search-field-input', '+251944108619;'),
+      await page.type('#chat-input', 'Server started'),
+      await page.click('#btn-send'),
     ])
 
   await page
     .$('a[href="#sms"]')
     .then((btn) => btn?.click)
     .then(async () => {
-      await sendSms().catch((err) => console.log("couldnt write number", err))
+      await sendSms().catch((err) => console.log('couldnt write number', err))
     })
     .catch(async () => {
       await login()
